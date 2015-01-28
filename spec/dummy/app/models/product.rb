a9ef34b52796db9fb30845133429f6441a71ab34
Kevin Bongart <contact@kevinbongart.net>
2015-01-27 22:44:07 -0500

    Nicer specs

--- spec/dummy/app/models/product.rb
+++ spec/dummy/app/models/product.rb
@@ -2,7 +2,10 @@ class Product < ActiveRecord::Base
   belongs_to :company
 
   validates :company, presence: true
+
   validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/ }
-  validates :name, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :legacy_code, uniqueness: { scope: :company }
+
+  validates :name, format: { with: /\A[a-zA-Z\s]+\z/ }
   validates :name, uniqueness: { scope: :company }
 end
--- spec/lib/valid_attribute_spec.rb
+++ spec/lib/valid_attribute_spec.rb
@@ -1,28 +1,44 @@
 require "rails_helper"
 
 describe "#valid_attribute?" do
-  it "heyo" do
+  it "validates one attribute" do
     company = Company.new
-    product = Product.new(company: company, name: "heyo")
+    product = Product.new(company: company, name: "A cool thing", legacy_code: "COOL_THING")
 
     expect(product.valid?).to be_falsey
 
     expect(product.valid_attribute?(:company)).to be_truthy
     expect(product.valid_attribute?(:name)).to be_truthy
     expect(product.valid_attribute?(:legacy_code)).to be_falsey
+  end
+
+  it "validates several attributes" do
+    company = Company.new
+    product = Product.new(company: company, name: "A cool thing", legacy_code: "COOL_THING")
+
+    expect(product.valid?).to be_falsey
 
     expect(product.valid_attribute?(:company, :name)).to be_truthy
-    expect(product.valid_attribute?(:company, :name, :legacy_code)).to be_falsey
-
     expect(product.valid_attribute?([:company, :name])).to be_truthy
+    expect(product.valid_attribute?(:company, :name, :legacy_code)).to be_falsey
+  end
 
-    expect(product.valid_attribute?(name: :format)).to be_truthy
-    expect(product.valid_attribute?(name: [:format, :uniqueness])).to be_truthy
+  it "validates on specific validators" do
+    company = Company.new
+    product_1 = Product.create(company: company, name: "A cool thing", legacy_code: "COOLTHING")
+    product_2 = Product.new(company: company, name: "A cool thing", legacy_code: "COOL_THING")
 
-    product = Product.new(company: company, name: "heyo&^%$")
+    expect(product_2.valid?).to be_falsey
 
-    expect(product.valid_attribute?(name: :format)).to be_falsey
-    expect(product.valid_attribute?(name: :uniqueness)).to be_truthy
-    expect(product.valid_attribute?(name: [:format, :uniqueness])).to be_falsey
+    expect(product_2.valid_attribute?(name: :format)).to be_truthy
+    expect(product_2.valid_attribute?(name: :uniqueness)).to be_falsey
+    expect(product_2.valid_attribute?(name: [:format, :uniqueness])).to be_falsey
+
+    expect(product_2.valid_attribute?(legacy_code: :format)).to be_falsey
+    expect(product_2.valid_attribute?(legacy_code: :uniqueness)).to be_truthy
+    expect(product_2.valid_attribute?(legacy_code: [:format, :uniqueness])).to be_falsey
+
+    expect(product_2.valid_attribute?(company: :presence, name: :format, legacy_code: :uniqueness)).to be_truthy
+    expect(product_2.valid_attribute?(company: :presence, name: :format, legacy_code: [:format, :uniqueness])).to be_falsey
   end
 end
